5065cf600d99a0766166e99d5bebf9e72e20ebf6
Peter Chen <manchesterutd7@gmail.com>
2022-10-18 15:57:38 +0400

    initial commit

---
+++ src/lib/components/Question.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Collapsible from 'spaper/components/Collapsible.svelte';
+  import Button from 'spaper/components/Button.svelte';
+
+  export let question;
+  export let onClick;
+</script>
+
+<Collapsible label={question.id.toString()}>
+  <p>{question.label}</p>
+  {#if !!question.answer}
+    <Button outline="success" on:click={onClick(question)}>
+      See the answer!
+    </Button>
+  {/if}
+</Collapsible>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Modal from 'spaper/components/Modal/Modal.svelte';
+  import Question from '$lib/components/Question.svelte';
+  import questions from '$lib/questions';
+
+  let showAnswer = false;
+  let selectedQuestion = { id: 0, label: '', answer: ''};
+</script>
+
+<div class="bingo">
+  <h1>Welcome to Linda Bingo</h1>
+  <div class="bingo-content">
+    {#each questions as question}
+      <Question question={question} onClick={(q) => { selectedQuestion = q; showAnswer = true }} />
+      {/each}
+    <Modal bind:active={showAnswer} title="Answer">
+      {selectedQuestion.answer}
+    </Modal>
+  </div>
+</div>
+
+<style>
+  .bingo {
+    text-align: center;
+    margin: 40px 20px;
+  }
+
+  .bingo-content {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 10px;
+  }
+</style>
