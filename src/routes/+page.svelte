5e6acc5ab41ea21eb1373d411faabf1d1bab6e11
Peter Chen <manchesterutd7@gmail.com>
2022-10-20 15:31:25 +0400

    feature: add image and update answer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,14 @@
   import Modal from 'spaper/components/Modal/Modal.svelte';
   import Question from '$lib/components/Question.svelte';
   import questions from '$lib/questions';
+  import hearts from '$lib/images/hearts.png';
 
   let showAnswer = false;
   let selectedQuestion = { id: 0, label: '', answer: ''};
 </script>
 
 <div class="bingo">
+  <img class="bingo-img" src={hearts} alt="hearts" />
   <h1>Welcome to the Linda Bingo</h1>
   <div class="bingo-content">
     {#each questions as question}
@@ -18,6 +20,7 @@
     </Modal>
   </div>
 </div>
+<a href="https://www.flaticon.com/free-icons/heart" title="heart icons">Heart icons created by Freepik - Flaticon</a>
 
 <style>
   .purple {
@@ -26,7 +29,17 @@
 
   .bingo {
     text-align: center;
-    margin: 40px 20px;
+    margin: 2rem 4rem;
+  }
+
+  .bingo-img {
+    width: 4rem;
+    border: none;
+    margin: 0 auto;
+  }
+
+  h1 {
+    margin: 0 0 2rem 0;
   }
 
   .bingo-content {
